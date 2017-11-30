6465a887d51ef708b26fb41b6b7f90e86e95ff96
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2017-11-30 17:02:14 +0100

    Add code:zadanie_9_2 to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// zadanie_9_1
+
 function getTriangleArea(a,h) {
     if (a<0 && h<0) {
         return 'Nieprawidłowe dane'
@@ -12,3 +14,17 @@ var triangle2Area = getTriangleArea(15, 20);
 console.log('Pole trójkąta to ' +triangle2Area);
 var triangle3Area = getTriangleArea(20, 25);
 console.log('Pole trójkąta to ' +triangle3Area);
+
+// zadanie_9_2
+
+var femaleNames = ['Asia','Kasia','Ola','Jola'];
+var maleNames = ['Piotrek','Marek','Arek','Jarek'];
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+var newName = 'Marian';
+if (allNames.indexOf(newName) === -1) {
+    var x = allNames.push(newName);
+} else {
+    console.log('Marian już tu jest');
+}
+console.log(allNames);
