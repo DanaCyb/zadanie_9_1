abbc4d019185c2f2fd408ac1d24d04a6828917a7
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2017-12-01 11:55:42 +0100

    Add code:zadanie_9_3 to scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,9 @@
-function getTriangleArea(a,h) {
-    if (a<0 && h<0) {
-        return 'Nieprawidłowe dane'
-    } else {
-        return a*h/2
-    }
-};
-console.log(getTriangleArea(10,6));
-var triangle1Area = getTriangleArea(10, 15);
-console.log('Pole trójkąta to ' +triangle1Area);
-var triangle2Area = getTriangleArea(15, 20);
-console.log('Pole trójkąta to ' +triangle2Area);
-var triangle3Area = getTriangleArea(20, 25);
-console.log('Pole trójkąta to ' +triangle3Area);
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
+var result1 = dinosaur.toUpperCase();
+// console.log(result1);
+var result2 = text.replace('Velociraptor', result1);
+// console.log(result2);
+var x = text.length/2;
+var result3 = result2.slice(0, x);
+console.log(result3);
