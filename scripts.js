7994056623f3d261bb3630168d2008a630d92edb
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2017-12-07 18:49:38 +0100

    Add code:zadanie_9_6 in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,6 @@
-function getTriangleArea(a,h) {
-    if (a<0 && h<0) {
-        return 'Nieprawidłowe dane'
-    } else {
-        return a*h/2
-    }
-};
-console.log(getTriangleArea(10,6));
-var triangle1Area = getTriangleArea(10, 15);
-console.log('Pole trójkąta to ' +triangle1Area);
-var triangle2Area = getTriangleArea(15, 20);
-console.log('Pole trójkąta to ' +triangle2Area);
-var triangle3Area = getTriangleArea(20, 25);
-console.log('Pole trójkąta to ' +triangle3Area);
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+add.addEventListener('click',function() {
+    var element = document.createElement('li');
+    element.innerHTML ='item'+(document.getElementsByTagName('li').length-0);
+    list.appendChild(element);});
